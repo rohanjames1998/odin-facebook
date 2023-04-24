8c8e54f0c072d194953619e563575517ac5af22b
Rohan James <rohanjames9282@gmail.com>
2023-04-24 16:57:28 +0530

    Change root to users#root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  devise_scope :user do
-    root "devise/sessions#new"
-  end
+  root "users#root"
 end
