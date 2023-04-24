0eadd360681487dd0c95962f8c1be3310710b662
Rohan James <rohanjames9282@gmail.com>
2023-04-24 17:21:21 +0530

    Change expectation to check redirection

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "#root" do
     context "If user is signed in" do
-      it "redirects to user home page" do
+      it "redirects to user's home page" do
         allow_any_instance_of(Devise::Controllers::Helpers).to receive(:user_signed_in?).and_return true
 
         get root_path
 
-        expect(response.body).to include("Home")
+        expect(response).to redirect_to(user_home_path)
       end
     end
 
@@ -18,7 +18,7 @@ RSpec.describe "Users", type: :request do
 
         get root_path
 
-        expect(response.body).to include("Sign Up")
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
