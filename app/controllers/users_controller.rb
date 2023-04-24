9ee2a147aa14000606c689e61bf207cf003d4bf5
Rohan James <rohanjames9282@gmail.com>
2023-04-24 17:22:24 +0530

    Add users#root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
   def home
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+  end
+
+  def root
+    if user_signed_in?
+      redirect_to user_home_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
