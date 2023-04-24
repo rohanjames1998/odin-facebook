7d102151f73ca75434f01d54eda048053c1ca992
Rohan James <rohanjames9282@gmail.com>
2023-04-24 16:56:09 +0530

    Add tests for users_controller#root

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "#root" do
+    context "If user is signed in" do
+      it "redirects to user home page" do
+        allow_any_instance_of(Devise::Controllers::Helpers).to receive(:user_signed_in?).and_return true
+
+        get root_path
+
+        expect(response.body).to include("Home")
+      end
+    end
+
+    context "When user is not signed in" do
+      it "redirects to sign up page" do
+        allow_any_instance_of(Devise::Controllers::Helpers).to receive(:user_signed_in?).and_return false
+
+        get root_path
+
+        expect(response.body).to include("Sign Up")
+      end
+    end
   end
 end
