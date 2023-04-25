749365d243783ca6d8ea0927cb9385734ac9c71f
Rohan James <rohanjames9282@gmail.com>
2023-04-25 13:37:47 +0530

    Fix uniqueness validation failing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe User, type: :model do
     it { should validate_length_of(:first_name).is_at_least(3).is_at_most(20) }
     it { should validate_presence_of(:last_name) }
     it { should validate_length_of(:last_name).is_at_least(3).is_at_most(20) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
     it { should validate_length_of(:password).is_at_least(8) }
   end
