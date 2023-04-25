104980f7752dfe1ddc511fac3685b8d2269d6b84
Rohan James <rohanjames9282@gmail.com>
2023-04-25 13:16:45 +0530

    Add validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_length_of(:first_name).is_at_least(3).is_at_most(20) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_length_of(:last_name).is_at_least(3).is_at_most(20) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(8) }
+  end
 end
