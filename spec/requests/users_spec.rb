0c612a1c8e3c10bd977ec0e48b204c9bc240bbcc
Rohan James <rohanjames9282@gmail.com>
2023-04-25 13:42:17 +0530

    Fix url generator error when testing #root

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "#root" do
+  let(:user) { FactoryBot.create(:user) }
     context "If user is signed in" do
       it "redirects to user's home page" do
         allow_any_instance_of(Devise::Controllers::Helpers).to receive(:user_signed_in?).and_return true
+        allow_any_instance_of(Devise::Controllers::Helpers).to receive(:current_user).and_return(user)
 
         get root_path
 
-        expect(response).to redirect_to(user_home_path)
+        expect(response).to redirect_to(user_home_path(user))
       end
     end
 
