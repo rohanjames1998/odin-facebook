cbafdd248b75874ad4059cfea22fe018c44d2057
Rohan James <rohanjames9282@gmail.com>
2023-04-26 14:28:55 +0530

    Update user factory to use faker gem

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
 
 
   factory :user do
-    first_name { "John" }
-    last_name { "Doe" }
-    password { "password" }
-    email { "john@example.com" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    password { Faker::Internet.password(min_length: 8) }
+    email { Faker::Internet.email }
   end
 
 end
