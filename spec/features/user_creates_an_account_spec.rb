1df307808c9c4da85a091024279560e5cc3688f5
Rohan James <rohanjames9282@gmail.com>
2023-04-27 12:28:10 +0530

    Add tests for when user creates an account

---
+++ spec/features/user_creates_an_account_spec.rb
@@ -0,0 +1,36 @@
+RSpec.describe "Sign Up", type: :feature do
+
+    let(:user)  { FactoryBot.build(:user) }
+
+  context "When signing up with valid credentials" do
+    it "redirects to home page" do
+      visit "/users/sign_up"
+
+      fill_in "First name", with: user.first_name
+      fill_in "Last name", with: user.last_name
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      fill_in "Password confirmation", with: user.password
+
+      click_button 'Sign up'
+
+      expect(page).to have_content 'Home'
+    end
+  end
+
+  context "When signing up with invalid credentials" do
+    it "shows an error message" do
+      visit "/users/sign_up"
+
+      fill_in "First name", with: user.first_name
+      fill_in "Last name", with: user.last_name
+      fill_in "Email", with: ""
+      fill_in "Password", with: ""
+      fill_in "Password confirmation", with: ""
+
+      click_button "Sign up"
+
+      expect(page).to have_selector "div#error_explanation"
+    end
+  end
+end
