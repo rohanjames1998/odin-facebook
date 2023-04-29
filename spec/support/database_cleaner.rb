42b1fbc18eef01a6eb777f91a707684b3975c821
Rohan <rohanjames9282@gmail.com>
2023-04-29 14:03:09 +0530

    Fix test database not cleaning after each test

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,15 @@
+RSpec.configure do |config|
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
