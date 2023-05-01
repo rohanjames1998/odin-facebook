d20641948704616bb156c143c370884d47a199d6
Rohan James <rohanjames9282@gmail.com>
2023-05-01 12:48:14 +0530

    Add params sanitizer

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,4 +6,9 @@ class NotificationsController < ApplicationController
     notification.save!
     render "users/home", flash.now[:alert]= "Sent Succesfully"
   end
+
+  private
+  def notification_params
+    params.require(:notification).permit(:type, :sender_id, :receiver_id)
+  end
 end
