3bb0a5cb7b52f6cab9fe96e5e7f7c023a900e877
Rohan James <rohanjames9282@gmail.com>
2023-05-01 12:45:29 +0530

    Update #create to build notifications for the receiver

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,9 @@
 class NotificationsController < ApplicationController
 
   def create
-    flash.now[:alert] = "Sent Successfully!"
+    user = User.find(params[:receiver_id])
+    notification = user.notifications.build(notification_params)
+    notification.save!
+    render "users/home", flash.now[:alert]= "Sent Succesfully"
   end
 end
