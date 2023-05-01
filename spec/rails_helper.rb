86d9649a557c78556cba6edae1c745d598af4ae1
Rohan James <rohanjames9282@gmail.com>
2023-05-01 12:42:34 +0530

    Update database cleaner config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,6 +69,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
 
 Shoulda::Matchers.configure do |config|
@@ -82,4 +91,3 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :feature
 end
 
-DatabaseCleaner.clean
