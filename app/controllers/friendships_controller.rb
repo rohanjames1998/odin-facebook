e631337ea32ebb887fc9da12e38ec04239ee88c9
Rohan James <rohanjames9282@gmail.com>
2023-05-03 13:25:37 +0530

    Add #create to friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,15 @@
 class FriendshipsController < ApplicationController
+
+  def create
+    friendship = Friendship.new(friendship_params)
+    friendship.notifications.build(params[:friendship][:notifications_attributes])
+    friendship.save
+    flash.now[:alert] = "Sent Successfully!"
+    render "users/index", status: :ok
+  end
+
+  private
+  def friendship_params
+    params.require(:friendship).permit(:requested_user_id, :request_sender_id, notifications_attributes: [:receiver_id, :sender_id])
+  end
 end
