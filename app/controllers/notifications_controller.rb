cef09e0dac6a53a5ab8ee9cb10c0f1497d582cd9
Rohan James <rohanjames9282@gmail.com>
2023-05-09 12:59:32 +0530

    Remove redundant controller methods

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,15 +1,2 @@
 class NotificationsController < ApplicationController
-
-  def create
-    user = User.find(params[:notification][:receiver_id])
-    notification = user.notifications.build(notification_params)
-    notification.save!
-    flash.now[:alert] = "Sent Successfully!"
-    render "users/home", status: :ok
-  end
-
-  private
-  def notification_params
-    params.require(:notification).permit(:notifiable_type, :notifiable_id, :sender_id, :receiver_id)
-  end
 end
