a9155dfda84d570d6810503d681a960e7c519e0e
Rohan James <rohanjames9282@gmail.com>
2023-05-16 14:51:37 +0530

    Add #create action for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,14 @@
 class PostsController < ApplicationController
+
+  def create
+    post = current_user.posts.build(post_params)
+    post.save!
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text_content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get "friends", to: "users#friends", as: :user_friends
   resources :users, only: [ :index, :show ]
   resources :friendships, only: [ :create, :update ]
+  resources :posts, only: [ :create ]
 
   delete 'friendship/:requested_user_id/request_sender_id', to: 'friendships#destroy', as: :cancel_request
 end
