15fe41b817954d5d978c8d740927d72741b79485
Rohan James <rohanjames9282@gmail.com>
2023-05-23 15:12:51 +0530

    Add more assertions

--- spec/features/post/creating_simple_post_spec.rb
+++ spec/features/post/creating_simple_post_spec.rb
@@ -5,16 +5,41 @@ RSpec.describe "Creating a simple text post", type: :feature, js: true do
     Capybara.current_driver = :selenium_chrome_headless
   end
   context "When user creates a valid text post" do
-    it "shows on their home page" do
-      user = FactoryBot.create(:user)
-      quote = Faker::Quote.famous_last_words
+    let(:user)  { FactoryBot.create(:user) }
+    let(:quote) { Faker::Quote.famous_last_words }
 
+    it "shows on their home page" do
       login(user)
 
       fill_in "post_text_content", with: quote
       click_button "Post"
 
       expect(page).to have_content(quote)
+    end
+
+    it "shows on the homepage of their friends" do
+      friend_one = FactoryBot.create(:user)
+      friend_two = FactoryBot.create(:user)
+      FactoryBot.create(:friendship, request_sender_id: user.id, requested_user_id: friend_one.id)
+      FactoryBot.create(:friendship, request_sender_id: friend_two.id, requested_user_id: user.id)
+
+      login(user)
+
+      fill_in "post_text_content", with: quote
+      click_button "Post"
+
+      assert_broadcast_on("home_#{friend_one.id}", quote)
+      assert_broadcast_on("home_#{friend_two.id}", quote)
+    end
+
+    it "does not show up on homepage of users who are not poster's friend" do
+      random_user = FactoryBot.create(:user)
+      login(user)
+
+      fill_in "post_text_content", with: quote
+      click_button "Post"
+
+      assert_no_broadcasts("home_#{random_user.id}")
     end
   end
 end
