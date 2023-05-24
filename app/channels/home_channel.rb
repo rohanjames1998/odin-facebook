99bfaf0f48e306043bafdae679206fc2f447aba7
Rohan James <rohanjames9282@gmail.com>
2023-05-24 12:05:25 +0530

    Add home channel

---
+++ app/channels/home_channel.rb
@@ -0,0 +1,5 @@
+class HomeChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "home_#{params[:room]}"
+  end
+end
---
+++ spec/channels/home_channel_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe HomeChannel, type: :channel do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
