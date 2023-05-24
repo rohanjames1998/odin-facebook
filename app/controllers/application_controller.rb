b8ee9dfd9ee13d5c41b859d7ad32063b88c034d8
Rohan James <rohanjames9282@gmail.com>
2023-05-24 12:03:17 +0530

    Set user id cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :user_signed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_user_id, if: :user_signed_in?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def set_user_id
+    cookies[:user_id] = current_user.id || nil
   end
 end
