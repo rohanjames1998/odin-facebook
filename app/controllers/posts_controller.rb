a14cf11e561b022f66186eb252f1c2dc63d9b02f
Rohan James <rohanjames9282@gmail.com>
2023-05-24 14:58:01 +0530

    Add logic to broadcast post to user and their friend after posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,11 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
-    respond_to do |format|
-      if @post.save
-        format.turbo_stream { render turbo_stream: turbo_stream.prepend('posts', @post)}
+    if @post.save
+      current_user.friends.each do |friend|
+        ActionCable.server.broadcast("home_#{friend.id}", @post.as_json(include: :user))
       end
+      ActionCable.server.broadcast("home_#{current_user.id}", @post.as_json(include: :user))
     end
   end
 
