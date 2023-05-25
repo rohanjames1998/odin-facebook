ea2115a75a9db8c6c35e031b384cfa8704051e49
Rohan James <rohanjames9282@gmail.com>
2023-05-25 11:14:50 +0530

    Update friendship factory to include status

--- spec/factories/friendship.rb
+++ spec/factories/friendship.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
   factory :friendship do
     requested_user_id { FactoryBot.create(:user).id }
     request_sender_id { FactoryBot.create(:user).id }
+    status { "accepted" }
   end
 end
