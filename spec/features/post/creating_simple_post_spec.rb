69880fa5893c0670aa9968b3f8fe3ae8b58fea26
Rohan James <rohanjames9282@gmail.com>
2023-05-27 12:46:16 +0530

    Seperate different feature specs

--- spec/features/post/creating_simple_post_spec.rb
+++ spec/features/post/creating_simple_post_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Creating a simple text post", type: :feature, js: true do
+RSpec.describe "Creating post", type: :feature, js: true do
   before do
     Capybara.current_driver = :selenium_chrome_headless
   end
@@ -13,34 +13,9 @@ RSpec.describe "Creating a simple text post", type: :feature, js: true do
 
       fill_in "post_text_content", with: quote
       click_button "Post"
+      wait_for_changes
 
       expect(page).to have_content(quote)
-    end
-
-    it "shows on the homepage of their friends" do
-      friend = FactoryBot.create(:user)
-      FactoryBot.create(:friendship, request_sender_id: user.id, requested_user_id: friend.id)
-
-      login(friend)
-
-      using_session("user") do
-        login(user)
-        fill_in "post_text_content", with: quote
-        click_button "Post"
-      end
-
-      expect(page).to have_content(quote)
-    end
-
-    it "does not show up on homepage of users who are not poster's friend" do
-      random_user = FactoryBot.create(:user)
-      login(random_user)
-      using_session("user") do
-        login(user)
-        fill_in "post_text_content", with: quote
-        click_button "Post"
-      end
-      expect(page).not_to have_content(quote)
     end
   end
 end
---
+++ spec/features/post/live_post_feed_for_friends_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "When user creates a post", type: :feature, js: true do
+
+  before do
+    Capybara.current_driver = :selenium_chrome_headless
+  end
+
+  context "and they have friends online" do
+    let(:user)  { FactoryBot.create(:user) }
+    let(:quote) { Faker::Quote.famous_last_words }
+
+    it "shows on the homepage of their friends" do
+      friend = FactoryBot.create(:user)
+      FactoryBot.create(:friendship, request_sender_id: user.id, requested_user_id: friend.id)
+
+      login(friend)
+
+      using_session("user") do
+        wait_for_changes
+        login(user)
+        wait_for_changes
+        fill_in "post_text_content", with: quote
+        click_button "Post"
+      end
+
+      expect(page).to have_content(quote)
+    end
+  end
+end
