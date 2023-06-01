8c26af93c8bb967903d75d159529f604e0f32fe1
Rohan James <rohanjames9282@gmail.com>
2023-06-01 14:29:32 +0530

    Add helpful comment

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,8 @@ class Friendship < ApplicationRecord
   validates :request_sender_id, presence: true
   enum :status, %i(pending accepted)
 
+  # Friendship has many notifications because there is a notification when someone sends a friend_req,
+  # and when someone accepts a friend_req.
   has_many :notifications, as: :notifiable, dependent: :delete_all
   belongs_to :requested_user, class_name: "User"
   belongs_to :request_sender, class_name: "User"
