5f2ec2468ad3ccd65554c0ebf1f338ed4cc01b4b
Rohan James <rohanjames9282@gmail.com>
2023-06-05 13:16:42 +0530

    Add feature test for commenting on post

---
+++ spec/features/comment/user_can_comment_on_post_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Commenting on a post", type: :feature, js: true do
+  before do
+    Capybara.current_driver = :selenium_chrome_headless
+  end
+
+  context "When user comments on a post" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:post) { FactoryBot.create(:post) }
+    let(:quote) { Faker::Quote.famous_last_words }
+
+    before do
+      allow(Post).to receive(:relevant_posts).and_return([post])
+      # disabling bullet gem because it is wrongly presenting errors.
+      Bullet.enable = false
+    end
+    it "shows their comment on the post" do
+      login(user)
+
+      click_button "Comment"
+      wait_for_changes
+      fill_in "comment_text_content", with: quote
+      wait_for_changes
+
+      within "#comments" do
+        expect(page).to have_content(quote)
+      end
+    end
+  end
+end
