f052dc5e89bc30d431f51c90dab774a5ee219af6
Rohan James <rohanjames9282@gmail.com>
2023-06-12 17:52:20 +0530

    Remove need to pass user_id to delete likes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   resources :comments, only: [ :create ]
 
   delete 'friendship/:requested_user_id/:request_sender_id', to: 'friendships#destroy', as: :cancel_request
-  delete 'like/:user_id/:likeable_type/:likeable_id', to: 'likes#destroy', as: :remove_like
+  delete 'like/:likeable_type/:likeable_id', to: 'likes#destroy', as: :remove_like
 end
