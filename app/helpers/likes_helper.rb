a0642cbecd74f3338a4367050d60e6daa73da50e
Rohan James <rohanjames9282@gmail.com>
2023-06-12 17:59:51 +0530

    Refactor #already_liked? not to use current_user variable.
    This helps us avoid errors when streaming likeable content with
    TurboStream. #already_liked? now uses a cookie with user_id instead of
    getting its arguments from a like object.

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,6 +1,9 @@
 module LikesHelper
-  def already_liked?(like)
-    if Like.where("user_id = ? and likeable_type = ? and likeable_id = ?", like.user_id, like.likeable_type, like.likeable_id).blank?
+  def already_liked?(likeable_type, likeable_id)
+    if Like.where("user_id = ? and likeable_type = ? and likeable_id = ?",
+                  cookies[:user_id],
+                  likeable_type,
+                  likeable_id).blank?
       return false
     end
     return true
--- spec/helpers/likes_helper_spec.rb
+++ spec/helpers/likes_helper_spec.rb
@@ -5,14 +5,16 @@ RSpec.describe LikesHelper, type: :helper do
     context "When user has already like the content" do
       it "returns true" do
         like = FactoryBot.create(:like)
-        expect(helper.already_liked?(like)).to eq(true)
+        request.cookies[:user_id] = like.user.id
+        expect(helper.already_liked?(like.likeable_type, like.likeable_id)).to eq(true)
       end
     end
     context "If the use hasn't liked the content before" do
       it "returns false" do
         # Because we are not persisting the like to database it should return false
         new_like = FactoryBot.build(:like)
-        expect(helper.already_liked?(new_like)).to eq(false)
+        request.cookies[:user_id] = new_like.user.id
+        expect(helper.already_liked?(new_like.likeable_type, new_like.likeable_id)).to eq(false)
       end
     end
   end
