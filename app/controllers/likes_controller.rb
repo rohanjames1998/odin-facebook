91e33dd5aa5ae44f08e1949d0f44d76eb5e60346
Rohan James <rohanjames9282@gmail.com>
2023-06-12 18:02:57 +0530

    Fix argument error caused by #already_liked? helper

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,27 @@
 class LikesController < ApplicationController
   def create
     like = current_user.likes.build(like_params)
-    if helpers.already_liked?(like)
+    if helpers.already_liked?(like.likeable_type, like.likeable_id )
       # In the unlikely case where user has disabled their js and the unlike button doesn't
       # show up this will prevent duplicate likes.
       head :forbidden
     else
-      like.build_notification(receiver_id: params[:like][:notification_attributes][:receiver_id], sender_id: params[:like][:notification_attributes][:sender_id])
+      like.build_notification(receiver_id: params[:like][:notification_attributes][:receiver_id],
+                              sender_id: current_user.id)
       like.save
       head :no_content
     end
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, likeable_type: params[:likeable_type], likeable_id: params[:likeable_id])
+    like = Like.find_by(user_id: current_user.id,
+                        likeable_type: params[:likeable_type],
+                        likeable_id: params[:likeable_id])
     like.destroy!
   end
 
   private
   def like_params
-    params.require(:like).permit(:likeable_type, :likeable_id, notification_attributes: [:receiver_id, :sender_id])
+    params.require(:like).permit(:likeable_type, :likeable_id, notification_attributes: [:receiver_id])
   end
 end
