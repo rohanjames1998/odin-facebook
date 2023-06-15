d2e517a897a261d1cb9e88f3d6d5bc3204509543
Rohan James <rohanjames9282@gmail.com>
2023-06-15 11:34:44 +0530

    Add ability for user to reply to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,23 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     post = Post.find(params[:comment][:post_id])
-     if @comment.save!
+    @comment.save
+     unless params[:comment].has_key?(:parent_comment_id)
       Turbo::StreamsChannel.broadcast_action_to(
         "post_#{params[:comment][:post_id]}",
         action: :append,
         target: "comments",
         partial: "comments/comment", locals: { comment: @comment, post: post}
+      )
+     else
+      parent_comment_replies_count = Comment.find(params[:comment][:parent_comment_id]).replies.count
+      Turbo::StreamsChannel.broadcast_action_to(
+        "post_#{params[:comment][:post_id]}",
+        action: :append,
+        target: "comment_#{params[:comment][:parent_comment_id]}",
+        partial: "comments/reply", locals: {comment: @comment,
+                                            post: post,
+                                            reply_count: parent_comment_replies_count}
       )
      end
   end
--- spec/features/comment/user_can_reply_to_comments_spec.rb
+++ spec/features/comment/user_can_reply_to_comments_spec.rb
@@ -9,26 +9,24 @@ RSpec.describe "Replying to a comment", type: :feature, js: true do
     let(:user) { FactoryBot.create(:user) }
     let(:post) { FactoryBot.create(:post) }
     let(:quote) { Faker::Quote.famous_last_words }
-    let(:comment) { FactoryBot.create(:comment)}
+    let(:comment) { FactoryBot.create(:comment, post_id: post.id)}
 
     before do
       allow(Post).to receive(:relevant_posts).and_return([post])
       # disabling bullet gem because it is wrongly presenting errors.
-      allow(post).to receive(:comments).and_return([comment])
       Bullet.enable = false
     end
     it "shows their reply under the comment" do
+      comment.post = post
       login(user)
-
       click_button "Comment"
-      wait_for_changes
       within "#comment_#{comment.id}" do
         click_button "Reply"
         fill_in "comment_text_content", with: quote
         click_button "Post Comment"
         wait_for_changes
-        expect(page).to have_content quote
       end
+      expect(page).to have_content quote
     end
   end
 end
