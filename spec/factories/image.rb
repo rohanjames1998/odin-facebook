cc9344c112cae3ec106494f603ca13b42e422fc2
Rohan James <rohanjames9282@gmail.com>
2023-06-23 01:53:33 +0530

    Fix image factory

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
 
   factory :image do
     picture { Rack::Test::UploadedFile.new('app/assets/images/test_img.png', 'image/png') }
-    post nil
     uploader_id { FactoryBot.create(:user).id }
+    imageable_id { FactoryBot.create(:post).id }
+    imageable_type { "Post" }
   end
  end
