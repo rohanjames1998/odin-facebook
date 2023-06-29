86fe2d7eb28ff4f814be557449fc5a1deef850e3
Rohan James <rohanjames9282@gmail.com>
2023-06-29 14:56:01 +0530

    Add devise omniauth to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i(facebook)
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
