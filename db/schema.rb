1bcc3c54fab2979b7da39b122bfbc3cc604fb5d8
Rohan James <rohanjames9282@gmail.com>
2023-06-29 14:51:41 +0530

    Add omniauth to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_25_081505) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_082904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -136,6 +136,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_25_081505) do
     t.datetime "updated_at", null: false
     t.string "first_name", limit: 20, default: "", null: false
     t.string "last_name", limit: 20, default: "", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
