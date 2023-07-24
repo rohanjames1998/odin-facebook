6f1617ddd8b2e4fdcbd47a7af5e669a4344ef3cc
Rohan James <rohanjames9282@gmail.com>
2023-07-24 17:44:26 +0530

    Add notification for when someone comments or replies to a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,47 @@
 class CommentsController < ApplicationController
   def create
+    post = Post.find(params[:comment][:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.build_text(content: params[:comment][:text][:content], author_id: current_user.id)
-    post = Post.find(params[:comment][:post_id])
-    @comment.save
-     unless params[:comment].has_key?(:parent_comment_id)
-      Turbo::StreamsChannel.broadcast_action_to(
-        "post_#{params[:comment][:post_id]}",
-        action: :append,
-        target: "comments",
-        partial: "comments/comment", locals: { comment: @comment, post: post}
-      )
+     if @comment.save
+      stream_comment(@comment, params, post)
+      send_notification(@comment, params[:comment][:parent_comment_id])
      else
-      parent_comment_replies_count = Comment.find(params[:comment][:parent_comment_id]).replies.count
-      Turbo::StreamsChannel.broadcast_action_to(
-        "post_#{params[:comment][:post_id]}",
-        action: :append,
-        target: "comment_#{params[:comment][:parent_comment_id]}",
-        partial: "comments/reply", locals: {comment: @comment,
-        post: post,
-        reply_count: parent_comment_replies_count}
-        )
+      # Flash notifications with error.
      end
   end
 
   private
     def comment_params
       params.require(:comment).permit(:post_id, :parent_comment_id, text_attributes: [ :content ])
+    end
+
+    def stream_comment(comment, params, post)
+      if params[:comment].has_key?(:parent_comment_id)
+        parent_comment_replies_count = Comment.find(params[:comment][:parent_comment_id]).replies.count
+        Turbo::StreamsChannel.broadcast_action_to(
+          "post_#{params[:comment][:post_id]}",
+          action: :append,
+          target: "comment_#{params[:comment][:parent_comment_id]}",
+          partial: "comments/reply", locals: {comment: @comment,
+          post: post,
+          reply_count: parent_comment_replies_count}
+          )
+       else
+        Turbo::StreamsChannel.broadcast_action_to(
+          "post_#{params[:comment][:post_id]}",
+          action: :append,
+          target: "comments",
+          partial: "comments/comment", locals: { comment: @comment, post: post}
+        )
+       end
+    end
+
+    def send_notification(comment, parent_id)
+      if parent_id == nil
+        comment.notifications.create(receiver_id: comment.post.author_id, sender_id: current_user.id)
+      else
+        comment.notifications.create(receiver_id: Comment.find(parent_id).author_id, sender_id: current_user.id)
+      end
     end
 end
