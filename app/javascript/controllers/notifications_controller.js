1d1ea6bca36f7c6c8775bf83dfb052e75673c56a
Rohan James <rohanjames9282@gmail.com>
2023-07-25 13:57:18 +0530

    Fix clicking notification btn  won't close notifications

--- app/javascript/controllers/notifications_controller.js
+++ app/javascript/controllers/notifications_controller.js
@@ -1,21 +1,39 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ 'notificationBox', "parentDiv", 'notificationTemplate' ]
+  static targets = [ 'notificationBox', "parentDiv", 'notificationTemplate', 'notificationBtn' ]
 
   connect() {
     this.outsideClickHandler = this.closeNotifications.bind(this);
   }
 
   openNotifications() {
-    if (this.hasNotificationBoxTarget) { //If notification box was hidden before
+    if (this.hasNotificationBoxTarget && this.notificationBoxTarget.classList.contains("hidden")) { //If notification box was hidden before
       this.notificationBoxTarget.classList.remove("hidden");
+      this.changeBtnAction();
     }
     else {
       this.showNotifications();
+      this.changeBtnAction();
     }
+    this.addOutsideClickListener();
+  }
+
+  addOutsideClickListener() {
     // We are using timeout func in order to avoid notification box being immediately hidden upon clicking bell icon.
     setTimeout(() => document.addEventListener("click", this.outsideClickHandler), 100);
+  }
+
+  changeBtnAction(){
+    const btn = this.notificationBtnTarget;
+    const action = btn.getAttribute("data-action");
+    btn.removeAttribute("data-action");
+    if (action == "notifications#openNotifications"){
+      btn.setAttribute("data-action", "notifications#closeNotifications")
+    }
+    else {
+      btn.setAttribute("data-action", "notifications#openNotifications")
+    }
   }
 
   showNotifications() {
@@ -29,8 +47,13 @@ export default class extends Controller {
     // sure user can interact with notifications inside the box).
     if(e.target !== this.notificationBoxTarget && !this.notificationBoxTarget.contains(e.target)){
       this.notificationBoxTarget.classList.add("hidden");
-      document.removeEventListener("click", this.outsideClickHandler);
+      this.changeBtnAction();
+      this.removeOutsideClickListener();
     }
+  }
+
+  removeOutsideClickListener() {
+    document.removeEventListener("click", this.outsideClickHandler);
   }
 
 }
